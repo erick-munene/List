873c5b27e80943b285562adb4dcc1cee6b19b993
Erick-Munene <munenewahome@gmail.com>
2016-05-27 15:20:43 +0300

    added the list view

---
+++ app/src/main/java/com/example/erick_king/list/ListView.java
@@ -0,0 +1,7 @@
+package com.example.erick_king.list;
+
+/**
+ * Created by erick-king on 27/05/16.
+ */
+public class ListView {
+}
--- app/src/main/java/com/example/erick_king/list/MainActivity.java
+++ app/src/main/java/com/example/erick_king/list/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.erick_king.list;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] foods = {"potato", "beans", "mango", "tomamo"};
+        ListAdapter listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foods);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        ListView listView1.setAdapter(listAdapter);
     }
+
+
+
+
 }
